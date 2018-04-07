9cc91e04db29407d3970593472f465491de46949
Anubhav Saxena <anubhavsaxena14992@gmail.com>
2018-04-07 17:39:31 +0530

    Update Assignment2.r

--- Assignment2.r
+++ Assignment2.r
@@ -4,20 +4,26 @@ if((x==0)&(y==0))
 {
   print("Lies on Origin")
 }else{
-  if((x>0)&(y>0))
+  if(x>0)
   {
-    print("First Quadrant")
+    if(y>0)
+    {
+      print("First Quadrant") 
+    }
+    if(y<0)
+    {
+      print("Forth Quadrant")
+    }
   }
-  if((x<0)&(y>0))
+  if((x<0))
   {
-    print("Second Quadrant")
-  }
-  if((x<0)&(y<0))
-  {
+    if(y>0)
+    {
+      print("Second Quadrant")
+    }
+    if(y<0)
+    {
     print("Third Quadrant")
+    }
   }
-  if((x>0)&(y<0))
-  {
-    print("Forth Quadrant")
-  }
-}
+}  
