75afa06f7e71d26ab2a2d13e3a4e081c4a1dff87
Anubhav Saxena <anubhavsaxena14992@gmail.com>
2018-04-07 23:06:35 +0530

    Update Assignment4.r

--- Assignment4.r
+++ Assignment4.r
@@ -5,13 +5,17 @@ len = nchar(sub('^0+','',sub('\\.','',a)))
 i = 1
 count = 0
 print(paste("length is",len))
+print(paste("a is",a))
 while(i<=len)
 {
-  if((a)%%(10^i) == n1)
+  print(paste("a %% is",a%%10))
+  if(a%%10 == n1)
   {
     count = count+1
     print(paste("i is",i))
   }
+  a = as.integer(a/10)
+  print(paste("a / is",a/10))
 i = i+1 
 }
 print(paste(n1," occurs", count, "times"))
