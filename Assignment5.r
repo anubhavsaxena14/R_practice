fbfbe6879995959f34823a6b518580d2964a06aa
Anubhav Saxena <anubhavsaxena14992@gmail.com>
2018-04-08 16:33:44 +0530

    Create Assignment5.r

---
+++ Assignment5.r
@@ -0,0 +1,13 @@
+fact = function(n)
+{	
+  if(n == 1)
+  {
+    return(1);
+  }else{
+    return(a*fact(a-1));	
+  }
+}
+n = readline("Enter a number")
+a = as.integer(n) 
+x = fact(1+(a-1)*3)
+print(paste("x is", x))
